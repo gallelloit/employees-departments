6be9035ecfd1d7adc879ff209ac0832eb5bcb225
Pedro Gallello Bonino <pgallello@Pedros-MacBook-Pro-2.local>
2026-05-02 13:10:15 +0200

    Remove duplicate title. Update gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import EmployeesPage from "./pages/EmployeesPage";
 function App() {
     return (
         <div>
-            <h1>Employees Dashboard</h1>
             <EmployeesPage />
         </div>
     );
