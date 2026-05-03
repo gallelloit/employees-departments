e80774ec6aecb77469b4b8bf82f46044423438e0
Pedro Gallello Bonino <pgallello@Pedros-MacBook-Pro-2.local>
2026-05-03 10:05:23 +0200

    Added assertion

--- src/test/java/com/gallelloit/employees/configuration/CorsConfigUnitTest.java
+++ src/test/java/com/gallelloit/employees/configuration/CorsConfigUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CorsConfigUnitTest {
 
     @Test
@@ -12,8 +14,12 @@ class CorsConfigUnitTest {
 
         WebMvcConfigurer configurer = corsConfig.corsConfigurer();
 
+        assertThat(configurer).isNotNull();
+
         CorsRegistry registry = new CorsRegistry();
 
         configurer.addCorsMappings(registry);
+
+        assertThat(registry).isNotNull();
     }
 }
